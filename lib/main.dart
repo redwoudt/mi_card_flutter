a801d1f55ebaf5c352fbb0fee1ab9f9a48897917
Ferdi Redelinghuys <ferdi.redelinghuys@vulamobile.com>
2020-09-29 21:10:25 +0200

    block challenge

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('Demo Hot Reload'),
-        ),
-        backgroundColor: Colors.orange,
-        body: Center(
-          child: Text('My name is Ferdi.'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.red,
-          child: Icon(Icons.add),
+        backgroundColor: Colors.teal,
+        body: Container(
+          margin: EdgeInsets.symmetric(
+            vertical: 30.0,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
         ),
       ),
     );
